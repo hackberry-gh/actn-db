48b3e5ed018eb8523dcbedc86de8ec5141496f95
Onur Uyar <me@onuruyar.com>
2014-10-03 11:03:41 +0100

    coffee to js

--- db/__setup.sql
+++ db/__setup.sql
@@ -2,7 +2,6 @@
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "plv8";
-CREATE EXTENSION IF NOT EXISTS "plcoffee";
 
 
 
@@ -14,24 +13,27 @@ CREATE TABLE plv8_modules(modname text primary key, load_on_start boolean, code 
 
 CREATE OR REPLACE FUNCTION plv8_startup() RETURNS void AS $$
   
-  load_module = (modname) ->
-    rows = plv8.execute("SELECT code from public.plv8_modules " + " where modname = $1", [modname])
-    r = 0
+  var code, load_module, r, rows;
 
-    while r < rows.length
-      code = rows[r].code
-      eval("(function(){" + code + "})")()
-      r++
-    return
+  load_module = function(modname) {
+    var code, r, rows;
+    rows = plv8.execute("SELECT code from public.plv8_modules " + " where modname = $1", [modname]);
+    r = 0;
+    while (r < rows.length) {
+      code = rows[r].code;
+      eval("(function(){" + code + "})")();
+      r++;
+    }
+  };
 
+  rows = plv8.execute("SELECT modname, code from public.plv8_modules where load_on_start");
 
-  # now load all the modules marked for loading on start
-  rows = plv8.execute("SELECT modname, code from public.plv8_modules where load_on_start")
-  r = 0
+  r = 0;
 
-  while r < rows.length
-    code = rows[r].code
-    eval("(function(){" + code + "})")()
-    r++  
+  while (r < rows.length) {
+    code = rows[r].code;
+    eval("(function(){" + code + "})")();
+    r++;
+  }
     
-$$ LANGUAGE plcoffee STABLE STRICT;
+$$ LANGUAGE plv8 STABLE STRICT;
