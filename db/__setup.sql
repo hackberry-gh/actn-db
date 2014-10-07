8ae9e973e22341e640757b307fecf95a9fa1a3c4
Onur Uyar <me@onuruyar.com>
2014-10-07 15:01:23 +0100

    testtesttest

--- db/__setup.sql
+++ db/__setup.sql
@@ -12,27 +12,19 @@ CREATE TABLE plv8_modules(modname text primary key, load_on_start boolean, code 
 
 CREATE OR REPLACE FUNCTION plv8_startup() RETURNS void AS $$
   
-  var code, load_module, r, rows;
+  var code, r, rows;
 
-  load_module = function(modname) {
-    var code, r, rows;
-    rows = plv8.execute("SELECT code from public.plv8_modules " + " where modname = $1", [modname]);
-    r = 0;
-    while (r < rows.length) {
-      code = rows[r].code;
-      eval("(function(){" + code + "})")();
-      r++;
-    }
-  };
-
-  rows = plv8.execute("SELECT modname, code from public.plv8_modules where load_on_start order by modname");
+  code = "";
+  
+  rows = plv8.execute("SELECT modname, code from public.plv8_modules order by modname");
 
   r = 0;
 
   while (r < rows.length) {
-    code = rows[r].code;
-    eval("(function(){" + code + "})")();
+    code += rows[r].code;
     r++;
   }
+  
+  eval("(function(){" + code + "})")();  
     
 $$ LANGUAGE plv8 STABLE STRICT;
