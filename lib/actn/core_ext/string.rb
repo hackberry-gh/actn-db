40cf53d1b12d0aab2898e2ddd9cbd81d5c5a0ca5
Onur Uyar <me@onuruyar.com>
2014-10-26 12:22:27 +0000

    use ruby URI class for String.to_domain

--- lib/actn/core_ext/string.rb
+++ lib/actn/core_ext/string.rb
@@ -1,4 +1,5 @@
 require 'oj'
+require 'uri'
 
 class ::String
   def to_json
@@ -9,6 +10,7 @@ class ::String
   end
   def to_domain
     return self unless self.start_with?("http")
-    self.match(/[http|http]:\/\/(\w*:\d*|\w*)\/?/)[1] rescue nil
+    # self.match(/[http|http]:\/\/(\w*:\d*|\w*)\/?/)[1] rescue nil
+    URI[self].host
   end
 end
